2e6a57c5923becd5717d82cb74b7f6368e43b74f
Benjamin-expertDev <benjaminmwambura@gmail.com>
2023-08-08 13:21:40 +0300

    path

---
+++ app.js
@@ -0,0 +1,6 @@
+const {name,age} =  require("./person")
+
+const benja = require("os")
+//console.log(benja.platform())
+const path = require("path")
+console.log(path.plaform())
---
+++ person.js
@@ -0,0 +1,7 @@
+const name = "Benja"
+const age = [23, 25]
+
+module.exports = {
+    name,
+    age,
+}
