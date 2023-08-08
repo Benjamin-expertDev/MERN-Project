b64fe28d43d0e455216f799e3911b00a51e35d49
Benjamin-expertDev <benjaminmwambura@gmail.com>
2023-08-08 15:48:23 +0300

    commit

--- node-server.js
+++ node-server.js
@@ -1,7 +1,19 @@
-const http = require("http")
-const server = http.createServer((req,res)=>{
-    console.log("ben")
-})
-server.listen("5000", "localhost", ()=>{
-    console.log("Server is runnig on port 5000")
-})
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+    //routes
+    if (req.url === "/") {
+        res.setHeader("Content-Type", "text/html"); 
+        res.write("<h2>Karibu home...</h2>");
+        res.end();
+    }
+        if (req.url === "/about") {
+        res.setHeader("Content-Type", "text/html"); 
+        res.write("<h3>About us</h3>");
+        res.end();
+    }
+});
+
+server.listen(5000, "localhost", () => {
+    console.log("Server is running on port 5000");
+});
