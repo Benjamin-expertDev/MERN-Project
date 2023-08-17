9ef337139f72e9c985ad4e9994db62cd5bb3c996
Benjamin-expertDev <benjaminmwambura@gmail.com>
2023-08-17 13:28:46 +0300

    tasks

--- MERN-TASK-APP/backend/controllers/taskController.js
+++ MERN-TASK-APP/backend/controllers/taskController.js
@@ -19,9 +19,23 @@ const getTasks = async (req, res) => {
         res.status(500).json({ msg: error.message });
     }
 };
-const getTask
+
+// Get a single task
+const getTask = async (req, res) => {
+    try {
+        const { id } = req.params; // Fix the typo here: req.params instead of res.params
+        const task = await Task.findById(id);
+        if(!task){
+            return res.status(404).json(`No task was found with the id:${id}`)
+        }
+        res.status(200).json(task);
+    } catch (error) {
+        res.status(500).json({ msg: error.message });
+    }
+};
 
 module.exports = {
     createTask,
+    getTasks,
     getTask,
 };
--- MERN-TASK-APP/backend/routes/taskRoute.js
+++ MERN-TASK-APP/backend/routes/taskRoute.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const Task = require("../model/taskModel");
-const { createTask, getTask } = require("../controllers/taskController");
+const { createTask, getTask, getTasks } = require("../controllers/taskController");
 const router = express.Router();
 
 
-router.get("/api/tasks", getTask)
+router.get("/api/tasks", getTasks)
 router.post("/api/tasks", createTask)
-
+router.get("/api/tasks/:id", getTask)
 
 
 
