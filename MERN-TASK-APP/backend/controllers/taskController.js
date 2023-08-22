df3bf2c5be1f00d8b204403f41c961c30e5932b5
Benjamin-expertDev <benjaminmwambura@gmail.com>
2023-08-22 16:50:41 +0300

    commit

--- MERN-TASK-APP/backend/controllers/taskController.js
+++ MERN-TASK-APP/backend/controllers/taskController.js
@@ -23,7 +23,7 @@ const getTasks = async (req, res) => {
 // Get a single task
 const getTask = async (req, res) => {
     try {
-        const { id } = req.params; // Fix the typo here: req.params instead of res.params
+        const { id } = req.params; 
         const task = await Task.findById(id);
         if(!task){
             return res.status(404).json(`No task was found with the id:${id}`)
@@ -33,9 +33,20 @@ const getTask = async (req, res) => {
         res.status(500).json({ msg: error.message });
     }
 };
+// Delete Task
+const deleteTask = async(req,res)=>{
+    const {id} = req.params
+    const task = await Task.findByIdAndDelete()
+    if(!task){
+        return res.status(404).json(`No task was found with the id:${id}`)
+    }
+    res.status(200).send("Task Deleted")
+    res.status(500).json({ msg: error.message });
 
+}
 module.exports = {
     createTask,
     getTasks,
     getTask,
+    deleteTask,
 };
--- MERN-TASK-APP/backend/routes/taskRoute.js
+++ MERN-TASK-APP/backend/routes/taskRoute.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const Task = require("../model/taskModel");
-const { createTask, getTask, getTasks } = require("../controllers/taskController");
+const { createTask, getTask, getTasks, deleteTask } = require("../controllers/taskController");
 const router = express.Router();
 
 
 router.get("/api/tasks", getTasks)
-router.post("/api/tasks", createTask)
+router.posts("/api/tasks", createTask)
 router.get("/api/tasks/:id", getTask)
+router.delete("/api/tasks/:id", deleteTask)
 
 
 
