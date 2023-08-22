b784b8696dc4eba2d2740a40bd1f3759c922a50c
Benjamin-expertDev <benjaminmwambura@gmail.com>
2023-08-22 16:59:17 +0300

    commit

--- MERN-TASK-APP/backend/controllers/taskController.js
+++ MERN-TASK-APP/backend/controllers/taskController.js
@@ -44,9 +44,28 @@ const deleteTask = async(req,res)=>{
     res.status(500).json({ msg: error.message });
 
 }
+// Update a Task
+const updateTask = async(req,res)=>{
+    try {
+        const {id}= req.params
+    const task =await Task.findByIdAndUpdate(
+        { _id:id}, req.body, { new: true}
+    )
+    runValidators:true;
+    res.status(200).json(task);
+        
+    } catch (error) {
+        res.status(500).json({ msg: error.message });
+        
+    }
+    
+
+
+}
 module.exports = {
     createTask,
     getTasks,
     getTask,
     deleteTask,
+    updateTask,
 };
--- MERN-TASK-APP/backend/routes/taskRoute.js
+++ MERN-TASK-APP/backend/routes/taskRoute.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const Task = require("../model/taskModel");
-const { createTask, getTask, getTasks, deleteTask } = require("../controllers/taskController");
+const { createTask, getTask, getTasks, deleteTask, updateTask } = require("../controllers/taskController");
 const router = express.Router();
 
 
 router.get("/api/tasks", getTasks)
-router.posts("/api/tasks", createTask)
+router.post("/api/tasks", createTask)
 router.get("/api/tasks/:id", getTask)
 router.delete("/api/tasks/:id", deleteTask)
-
+router.put("/api/tasks/:id", updateTask)
 
 
 module.exports = router
