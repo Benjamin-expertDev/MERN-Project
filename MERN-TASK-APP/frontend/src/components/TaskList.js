e82b3fa287c6f28dcfeb4ce035375a46d11c84a5
Benjamin-expertDev <benjaminmwambura@gmail.com>
2023-09-06 17:30:49 +0300

    commit

--- MERN-TASK-APP/frontend/src/components/TaskList.js
+++ MERN-TASK-APP/frontend/src/components/TaskList.js
@@ -1,12 +1,16 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Task from "./Task"
 import TaskForm from "./TaskForm"
 import { toast } from "react-toastify";
 import { URL } from "../App";
+import loadingImg from "../assets/loader.gif"
 
 //http://localhost:5000/api/tasks
 const TaskList = () => {
+    const [tasks, setTasks] = useState([])
+    const [completedTasks, setCompletedTasks] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
     const [formData, setFormData] = useState({
         name:"",
         completed: false
@@ -17,6 +21,24 @@ const TaskList = () => {
     const{name, value} = e.target
     setFormData({...formData, [name]:value})
     };
+
+    const getTasks = async() =>{
+        setIsLoading(true)
+        try {
+           const {data} = await axios.get(`${URL}/api/tasks`);
+           setTasks(data)
+           console.log(data);
+           setIsLoading(false)
+        } catch (error) {
+            toast.error(error.message)
+            console.log(error);
+           
+            setIsLoading(false)
+        }
+    };
+    useEffect(()=>{
+        getTasks()},[]        
+    )
     const createTask = async(e) =>{
         e.preventDefault()
         if(name===""){
@@ -26,6 +48,14 @@ const TaskList = () => {
             await axios.post(`${URL}/api/tasks`, formData)
             setFormData({...formData, name:""})
             toast.success("Task Added Successfully")
+        } catch (error) {
+            toast.error(error.message)
+        }
+    };
+    const deleteTask = async(id) =>{
+        try {
+            await axios.delete(`${URL}/api/tasks/${id}`)
+            getTasks()
         } catch (error) {
             toast.error(error.message)
         }
@@ -43,7 +73,26 @@ const TaskList = () => {
             </p>
         </div>
         <hr />
-        <Task/>
+        {
+            isLoading && (
+                <div className="--flex-center">
+                    <img src={loadingImg} alt="Loading"/>
+
+                    </div>
+            )
+        }
+        {
+            !isLoading && tasks.length === 0?(
+                <p className="--py">No task added. Please add a task</p>
+            ) : (
+                <>
+                {tasks.map((task, index)=>{
+                    return (<Task key={task._id} task={task} index={index} deleteTask={deleteTask} />)
+                })}
+                </>          
+            )
+        }
+        
     </div>
   )
 }
